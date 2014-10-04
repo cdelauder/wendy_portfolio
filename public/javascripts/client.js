f281b43aaf4d564aff807726a07972e863542fce
cdelauder <cdelauder@gmail.com>
2014-10-03 22:56:58 -0700

    layout is mostly there, as is the js. just need to swap out the photos for higher resolution versions and asjust the color scheme

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -3,6 +3,8 @@ $(document).ready(bindListeners)
 function bindListeners () {
   $('.link-button').on('mouseover', showNavigation)
   $('.navigation-links').on('mouseout', hideNavigation)
+  $('body').on('click', '.pdf', appendPdf)
+  $('body').on('click', '.close', cancel)
 }
 
 function showNavigation () {
@@ -12,4 +14,19 @@ function showNavigation () {
 
 function hideNavigation () {
   $('.navigation-links').css('display', 'none') 
+}
+
+function appendPdf (e) {
+  e.preventDefault()
+  $('html').append('<div class="shadow"></div>')
+  console.log(e.target.pathname)
+  $('.body').css('display', 'none')
+  $('body').prepend('<div class="inline-pdf"><button class="close">Close</button><iframe src="'+ e.target.pathname +'" class="iframe"></iframe></div>')
+  $('.close').one('click', cancel)
+}
+
+function cancel () {
+  $('.body').css('display', 'block')
+  $('.shadow').remove()
+  $('.inline-pdf').remove()
 }
